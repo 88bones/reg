728dbbdeb05adbbd384aaa771ef2669c79e7b5b0
88bones <123712642+88bones@users.noreply.github.com>
2023-07-22 18:36:46 +0545

    main

---
+++ insert.php
@@ -0,0 +1,20 @@
+
+<?php
+$name=$_POST['name'];
+$address=$_POST['address'];
+$phone=$_POST['phone'];
+//$gender=$_POST['gender'];
+$faculty=$_POST['faculty'];
+$remarks=$_POST['remarks'];
+$server="localhost";
+$username="root";
+$password="";
+$db="reg";
+$conn=mysqli_connect($server,$username,$password,$db);
+if(isset($conn)){
+    echo 'connection success "localhost/bca/index.php"';
+}else{
+    echo 'connectuon no';
+}
+
+?>
